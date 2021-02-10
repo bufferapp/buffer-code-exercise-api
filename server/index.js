af77cc2f55710052fdf253b7320972d9bd9507a2
Colin Ross <colin@colinscape.com>
2021-02-10 12:03:19 +0000

    New endpoint and supported states

--- server/index.js
+++ server/index.js
@@ -16,6 +16,33 @@ app.get('/getTweets', (req, res) => {
   res.json(tweets)
 })
 
+app.get('/kubectl', (req, res) => {
+  const N_STATES = 4;
+  const state = req.query.state ? Number(req.query.state) : Math.floor(Math.random() * N_STATES)
+
+  switch (state) {
+    case 1:
+      // Simulate permissions error
+      res.set('Content-Type', 'text/html').send(401, `<html>You do not have permission to access that content</html>`)
+      break;
+
+    case 2:
+      // Simulate generic server error
+      res.set('Content-Type', 'text/plain').send(500, 'Something went wrong')
+      break;
+
+    case 3:
+      // Simulate timeout
+      res.set('Content-Type', 'text/plain').send(504, `Unable to connect to the server: dial tcp 54.209.35.138:443: i/o timeout`)
+
+    default:
+      // Regular output
+      const output = require('./kubectl');
+      res.set('Content-Type', 'text/plain').send(200, output);
+      break;
+  }
+})
+
 database.init((err) => {
   if (err) {
     throw new Error(err)
